d2514cdb0f022e034564e9d82629622974fad737
Javier Rodríguez <160599950+ItsJavii28xX@users.noreply.github.com>
2025-05-14 10:33:34 +0200

    Update app.js

--- CapitalHub/Backend/app.js
+++ CapitalHub/Backend/app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+swaggerSetup(app);
+
 app.get('/', (req, res) => {
   res.json({ ok: true, message: 'CapitalHub Backend up!' });
 });
