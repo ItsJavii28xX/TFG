5f4da27034d532e4860c98489ed37e5a3872df2e
Javier Rodríguez <frodsot2402@g.educaand.es>
2025-05-20 21:08:14 +0200

    Deployment 2 v4

--- CapitalHub/Frontend/server.ts
+++ CapitalHub/Frontend/server.ts
@@ -25,7 +25,7 @@ export function app(): express.Express {
     index: 'index.html',
   }));
 
-  // All regular routes use the Angular engine cambio
+  // All regular routes use the Angular engine cambio 2
   server.get('**', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
