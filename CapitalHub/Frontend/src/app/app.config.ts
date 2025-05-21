7ece23164546eb0648f3e9f1025e8451f988d90a
Javier Rodríguez <frodsot2402@g.educaand.es>
2025-05-21 10:16:01 +0200

    login and reset password v2

--- CapitalHub/Frontend/src/app/app.config.ts
+++ CapitalHub/Frontend/src/app/app.config.ts
@@ -1,10 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter }    from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideZoneChangeDetection } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule }    from '@angular/material/input';
+import { MatButtonModule }   from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule }     from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { TokenInterceptor }  from './interceptors/token.interceptor';
+import { routes }            from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+
+    importProvidersFrom(
+      HttpClientModule,
+      ReactiveFormsModule,
+      MatInputModule,
+      MatButtonModule,
+      MatCheckboxModule,
+      MatCardModule,
+      MatSnackBarModule
+    ),
+
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+  ]
 };
